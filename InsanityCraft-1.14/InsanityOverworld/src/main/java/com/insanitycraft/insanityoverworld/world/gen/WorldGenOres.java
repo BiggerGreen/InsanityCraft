b22ad1055572e50d6916404082ddbc022dbab494
Corrupted Link <marcel.mathiss@gmail.com>
2019-08-22 00:57:32 +0200

    small oregen stat update

--- InsanityCraft-1.14/InsanityOverworld/src/main/java/com/insanitycraft/insanityoverworld/world/gen/WorldGenOres.java
+++ InsanityCraft-1.14/InsanityOverworld/src/main/java/com/insanitycraft/insanityoverworld/world/gen/WorldGenOres.java
@@ -9,36 +9,45 @@ import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import static com.insanitycraft.insanityoverworld.init.InsanityBlocks.*;
+import static net.minecraft.world.gen.GenerationStage.Decoration.UNDERGROUND_ORES;
+import static net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType.NATURAL_STONE;
 
 public class WorldGenOres {
 
-	private static final CountRangeConfig amethystConfig = new CountRangeConfig(3, 5, 0, 14);
-	private static final int amethystVeinSize = 4;
-	private static final CountRangeConfig rubyConfig = new CountRangeConfig(1, 0, 0, 10);
+	private static final CountRangeConfig amethystConfig = new CountRangeConfig(8, 0, 0, 25);
+	private static final int amethystVeinSize = 5;
+	private static final CountRangeConfig rubyConfig = new CountRangeConfig(3, 0, 0, 20);
 	private static final int rubyVeinSize = 2;
-	private static final CountRangeConfig titaniumConfig = new CountRangeConfig(4, 5, 0, 20);
-	private static final int titaniumVeinSize = 5;
-	private static final CountRangeConfig uraniumConfig = new CountRangeConfig(4, 5, 0, 20);
-	private static final int uraniumVeinSize = 5;
-
+	private static final CountRangeConfig titaniumConfig = new CountRangeConfig(1, 0, 0, 5);
+	private static final int titaniumVeinSize = 2;
+	private static final CountRangeConfig uraniumConfig = new CountRangeConfig(1, 0, 0, 5);
+	private static final int uraniumVeinSize = 2;
 
 
 	public static void setupOreGen() {
 		for(Biome biome : ForgeRegistries.BIOMES.getValues()) {
-			if (  biome.getCategory() == Biome.Category.THEEND)
-			{
+			if(biome.getCategory() == Biome.Category.THEEND) {
+				//END ores
 				continue;
 			}
-			if (  biome.getCategory() == Biome.Category.NETHER)
-			{
+			if(biome.getCategory() == Biome.Category.NETHER) {
+				//NETHER ores
 				continue;
 			}
 
-			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, amethystOre.getDefaultState(), amethystVeinSize), Placement.COUNT_RANGE, amethystConfig));
-			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, rubyOre.getDefaultState(), rubyVeinSize), Placement.COUNT_RANGE, rubyConfig));
-			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, titaniumOre.getDefaultState(), titaniumVeinSize), Placement.COUNT_RANGE, titaniumConfig));
-			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, uraniumOre.getDefaultState(), uraniumVeinSize), Placement.COUNT_RANGE, uraniumConfig));
+			biome.addFeature(UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(NATURAL_STONE, amethystOre.getDefaultState(), amethystVeinSize), Placement.COUNT_RANGE, amethystConfig));
+			biome.addFeature(UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(NATURAL_STONE, rubyOre.getDefaultState(), rubyVeinSize), Placement.COUNT_RANGE, rubyConfig));
+			biome.addFeature(UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(NATURAL_STONE, titaniumOre.getDefaultState(), titaniumVeinSize), Placement.COUNT_RANGE, titaniumConfig));
+			biome.addFeature(UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(NATURAL_STONE, uraniumOre.getDefaultState(), uraniumVeinSize), Placement.COUNT_RANGE, uraniumConfig));
+
+			addMobOres(biome);
+			
 		}
+	}
+
+
+	private static void addMobOres(Biome biome) {
+
 	}
 
 }
